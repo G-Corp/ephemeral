69a79069515f753192665593fc6ee13f94bc880a
glejeune <gregoire.lejeune@free.fr>
2016-05-27 11:50:49 +0200

    Update dependencies versions

--- Makefile
+++ Makefile
@@ -4,11 +4,10 @@ DEP_PLUGINS = mix.mk
 BUILD_DEPS = mix.mk
 ELIXIR_VERSION = ~> 1.2
 ELIXIR_BINDINGS = ostemp tempdir tempfile
-
 dep_mix.mk = git https://github.com/botsunit/mix.mk.git master
 
 DEPS = bucs
-dep_bucs = git https://github.com/botsunit/bucs.git master
+dep_bucs = git https://github.com/botsunit/bucs.git 0.0.1
 
 DOC_DEPS = edown
 dep_edown = git https://github.com/botsunit/edown.git master
--- mix.exs
+++ mix.exs
@@ -1,22 +1,22 @@
 defmodule Tempfile.Mixfile do
-	use Mix.Project
+  use Mix.Project
 
-	def project do
-		[app: :tempfile,
-		 version: "1.0.1-pre",
-		 elixir: "~> 1.2",
-		 build_embedded: Mix.env == :prod,
-		 start_permanent: Mix.env == :prod,
-		 deps: deps]
-	end
+  def project do
+    [app: :tempfile,
+     version: "1.0.1",
+     elixir: "~> 1.2",
+     build_embedded: Mix.env == :prod,
+     start_permanent: Mix.env == :prod,
+     deps: deps]
+  end
 
-	def application do
-		[applications: [:bucs]]
-	end
+  def application do
+    [applications: [:bucs]]
+  end
 
-	defp deps do
-		[ 
-			{:bucs, ~r/.*/, git: "https://github.com/botsunit/bucs.git", branch: "master"},
-		]
-	end
+  defp deps do
+    [ 
+      {:bucs, ~r/.*/, git: "https://github.com/botsunit/bucs.git", tag: "0.0.1"},
+    ]
+  end
 end
