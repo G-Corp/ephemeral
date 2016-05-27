eb3d876ce7b493d8121206ba61362f3f02e08fc1
glejeune <gregoire.lejeune@free.fr>
2016-05-27 11:52:43 +0200

    Bump version to 1.0.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Tempfile.Mixfile do
 
   def project do
     [app: :tempfile,
-     version: "1.0.1",
+     version: "1.0.2",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
